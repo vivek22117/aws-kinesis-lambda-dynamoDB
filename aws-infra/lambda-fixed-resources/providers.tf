6d136898eca4a0e42f5ad0c05abcc8b9de4de35d
vivek22117 <vivekmishra22117@gmail.com>
2021-08-07 19:30:40 +0530

    updating TF providers file

--- aws-infra/lambda-fixed-resources/providers.tf
+++ aws-infra/lambda-fixed-resources/providers.tf
@@ -3,7 +3,6 @@
 ####################################################
 provider "aws" {
   region  = var.default_region
-  profile = var.profile
 }
 
 
@@ -42,7 +41,6 @@ terraform {
 
 
   backend "s3" {
-    profile = "qa-admin"
     region  = "us-east-1"
     encrypt = "true"
   }
