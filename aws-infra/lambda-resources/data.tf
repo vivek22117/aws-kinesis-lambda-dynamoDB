71fdc79b4f375ae7b401a3f5a9b338ca6dbafb51
vivek22117 <vivekmishra22117@gmail.com>
2021-08-26 13:21:50 +0530

    removing unwatned TF script

--- aws-infra/lambda-resources/data.tf
+++ aws-infra/lambda-resources/data.tf
@@ -2,16 +2,6 @@
 #       Configuration to fetch remote state       #
 #       of artifactory bucket for deployment      #
 ###################################################
-data "terraform_remote_state" "backend" {
-  backend = "s3"
-
-  config = {
-    bucket  = "${var.s3_bucket_prefix}-${var.environment}-${var.default_region}"
-    key     = "state/${var.environment}/backend/terraform.tfstate"
-    region  = var.default_region
-  }
-}
-
 data "terraform_remote_state" "s3_buckets" {
   backend = "s3"
 
